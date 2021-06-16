2fc18b33724516800d93bd8382f861097d43b535
Andy Ye <ye.andyy@gmail.com>
2021-06-16 17:25:15 -0400

    Initial commit

---
+++ functions.js
@@ -0,0 +1,29 @@
+//playerObject
+
+//computerObject
+
+//create these with factories
+
+
+//Gameboard object
+//Array to hold values. Change the values of the array to X or O.
+
+//displayController
+//Assign the indexes of the array to a CSS grid item
+//Change the CSS when an X or O is present in the array
+//3x3 grid, items will be either 'X' or 'O'
+
+//Gameflow
+//User picks X or O. Computer will be assigned the other
+//Randomly assign either the computer or the player to start first
+// --> extra: if this is not the first game, let the winning player start first
+//First move will commence
+//check for move validity
+//Check win condition
+//The user that did not go first will choose a move
+//check for move validity
+//Recheck win condition
+//second move commence, recheck, third move, recheck, etc
+//Win condition satisified
+//display message for winner
+//--> extra: Create a button to play again
